02c6d7496399355e83cc59c25dabfc2f7fa26268
Mithun Chandra Howlader <mithun@primetechbd.com>
2025-03-27 11:43:50 +0600

    Update script

--- script/UpdateMySQLSystemdIP.sh
+++ script/UpdateMySQLSystemdIP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SERVER_IP=./GetServerIP.sh
-SERVER_IP=$(./GetServerIP.sh)
+SERVER_IP=$(../GetServerIP.sh)
 CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
 MYSQL_SERVICE=$(/usr/local/bin/check-mysql.sh)
 
