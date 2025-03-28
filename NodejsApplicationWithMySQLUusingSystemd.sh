2fb41080dc2832a92d526cf9677731098c5f20a2
Mithun Chandra Howlader <mithun@primetechbd.com>
2025-03-28 13:17:22 +0600

    script update

--- NodejsApplicationWithMySQLUusingSystemd.sh
+++ NodejsApplicationWithMySQLUusingSystemd.sh
@@ -45,7 +45,7 @@ sudo mv -f db/mysql-check.service /etc/systemd/system/mysql-check.service
 #STEP-3:
 sudo apt-get update
 sleep 5
-sudo apt-get install mysql-server --y
+sudo apt-get install -y mysql-server
 
 # Reload systemd to recognize the new service
 sudo systemctl daemon-reload
