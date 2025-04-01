6465955b10f0a655b8c0ef35e4226bf4b4fc5fdc
Mithun Chandra Howlader <mithun@primetechbd.com>
2025-04-01 18:36:00 +0600

    script update

--- NodejsApplicationWithMySQLUusingSystemd.sh
+++ NodejsApplicationWithMySQLUusingSystemd.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Exit script on any error and catch errors in piped commands
-#set -e
-#set -o pipefail
-
 LOG_FILE="install.log"
 exec > >(tee -a "$LOG_FILE") 2>&1  # Log output to file
 
